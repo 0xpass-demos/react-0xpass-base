cfce06b80e0f74fc0c74f0fa5bc862ccbb379293
Mana <manas@omlabs.xyz>
2023-06-14 13:45:59 +0530

    updated code

--- src/index.tsx
+++ src/index.tsx
@@ -5,66 +5,79 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 import '0xpass/style.css';
-import { PassProvider, connectorsForWallets, createClient } from "0xpass";
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+import {PassProvider, connectorsForWallets, createClient} from "0xpass";
+import {configureChains, createConfig, WagmiConfig} from 'wagmi';
+import {mainnet, polygon, optimism, arbitrum, goerli} from 'wagmi/chains';
+import {publicProvider} from 'wagmi/providers/public';
 import App from './App';
-import {ledgerWallet, metaMaskWallet, googleMagicWallet} from "0xpass/wallets";
+import { metaMaskWallet, googleMagicWallet, twitterMagicWallet, rainbowWallet, trustWallet} from "0xpass/wallets";
 
-
-
-const projectId = "dummuy"
-const apiKey = "dummy"
-
-
-const { chains, publicClient } = configureChains(
-    [mainnet, polygon, optimism, arbitrum],
+const {chains, publicClient, webSocketPublicClient} = configureChains(
+    [
+        mainnet,
+        polygon,
+        optimism,
+        arbitrum,
+        ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ],
     [publicProvider()]
 );
 
-const passClient=createClient({
-    apiKey: apiKey,
-    chains,
-});
+
+const projectId = process.env.REACT_APP_PROJECT_ID || ""
+const apiKey = process.env.REACT_APP_API || ""
+
 
 const connectors = connectorsForWallets([
     {
         groupName: "Social",
         wallets: [
-            // To use magic wallet, do enable magic sdk and add creds on ui
-            // googleMagicWallet({
-            //     chains,
-            //     apiKey: apiKey,
-            // }),
+            googleMagicWallet({
+                apiKey: apiKey,
+                chains
+            }),
+            twitterMagicWallet({
+                apiKey: apiKey,
+                chains
+            })
         ],
     },
     {
-        groupName: "Others",
+        groupName: "EOA",
         wallets: [
-            metaMaskWallet({ projectId, chains }),
-            ledgerWallet({ projectId, chains }),
+            metaMaskWallet({projectId, chains}),
+            rainbowWallet({projectId, chains}),
+            trustWallet({projectId, chains}),
+
         ],
     },
 ]);
 
 const wagmiConfig = createConfig({
-    connectors: connectors,
+    connectors,
     publicClient,
+    webSocketPublicClient,
 });
 
 
+const passClient = createClient({
+    apiKey: apiKey,
+    chains,
+});
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <WagmiConfig config={wagmiConfig}>
-      <PassProvider client={passClient}>
-        <App />
-      </PassProvider>
-    </WagmiConfig>
+    <React.StrictMode>
+        <WagmiConfig config={wagmiConfig}>
+            <PassProvider client={passClient}>
+                <App/>
+            </PassProvider>
+        </WagmiConfig>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
